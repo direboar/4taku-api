e72b6831a2119ced72f00102d0a4bc0d424f8bb7
direboar <direboar@example.com>
2022-05-18 23:06:53 +0900

    ナーガ追加

--- default_tiertable_2022-05-10_130242.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS tiertable;
-CREATE TABLE tiertable(
-    id integer,
-    name character varying(128),
-    ownerid integer,
-    tiers jsonb,
-    updatedat timestamp without time zone,
-    PRIMARY KEY(id)
-);
